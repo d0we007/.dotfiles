d7fa91ef569b66efb87f3ea036aceea794610b24
d0we007 <jfhkvceyzgsufga6ners@gmail.com>
2024-03-10 04:16:29 -0500

    modified:   config/nvim/.config/nvim/lua/mika/remap.lua

--- config/nvim/.config/nvim/lua/mika/remap.lua
+++ config/nvim/.config/nvim/lua/mika/remap.lua
@@ -45,8 +45,7 @@ vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/the
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 -- General
-vim.keymap.set("n", "<leader>q", ":!zathura <C-r>expand('%:r')<cr>.pdf &<cr>");
-
+vim.keymap.set("n", "<leader>q", ":!zathura <C-r>=expand('%:r')<cr>.pdf &<cr>")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
